dfb75bc69cb861b3421aa14bacbd3d64f15de292
YaBoiEazyYT <74844479+YaBoiEazyYT@users.noreply.github.com>
2023-10-08 00:32:29 -0500

    cha

--- stuff.js
+++
@@ -1,4 +0,0 @@
-document.body.innerHTML = "<a href=\"./index.html\">Home</a>"
-document.body.innerHTML = "<a href=\"./contacts.html\">Contacts</a>"
-document.body.innerHTML = "<a href=\"./acknowledgements.html\">Acknowledgements</a>"
-document.body.innerHTML = "<a href=\"./skills.html\">Skills</a>"
